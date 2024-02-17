9cd2ac17f116eb11eb2ed3057270d522da0e080e
alaakorda <alaazakaria436@gmail.com>
2024-02-17 03:39:24 +0200

    code refactor

--- lib/services/get_all_product.dart
+++ lib/services/get_all_product.dart
@@ -1,23 +1,15 @@
-import 'dart:convert';
+import 'package:store/helper/api.dart';
 import 'package:store/models/get_model.dart';
-import 'package:http/http.dart' as http;
 
 class AllProductService {
   Future<List<ProductModel>> getAllProduct() async {
-    http.Response response =
-        await http.get(Uri.parse('https://fakestoreapi.com/products'));
-    if (response.statusCode == 200) {
-  List<dynamic> data = jsonDecode(response.body);
-  List<ProductModel> productsList = [];
-  for (int i = 0; i < data.length; i++) {
-    productsList.add(ProductModel.fromJson(data[i]));
-  }
-  return productsList;
-}
-else {
-      throw Exception(
-          'there is aproblem with status code ${response.statusCode}');
+    List<dynamic> data =
+        await Api().get(url: 'https://fakestoreapi.com/products');
+    List<ProductModel> productsList = [];
+
+    for (int i = 0; i < data.length; i++) {
+      productsList.add(ProductModel.fromJson(data[i]));
     }
+    return productsList;
   }
-  
 }
