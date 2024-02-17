6718bbf272efe8d5ec14a2c40873d0692991a84f
alaakorda <alaazakaria436@gmail.com>
2024-02-17 03:49:11 +0200

    refactor code

--- lib/services/get_all_categorice.dart
+++ lib/services/get_all_categorice.dart
@@ -2,17 +2,12 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 
+import '../helper/api.dart';
+
 class AllCategoriceServices {
   Future<List<dynamic>> getAllCategorice() async {
-    http.Response response = await http
-        .get(Uri.parse('https://fakestoreapi.com/products/categories'));
-    if (response.statusCode == 200) {
-  List<dynamic> data = jsonDecode(response.body);
-  return data;
-}
-else {
-      throw Exception(
-          'there is aproblem with status code ${response.statusCode}');
-    }
+    List<dynamic> data =
+        await Api().get(url: 'https://fakestoreapi.com/products/categories');
+    return data;
   }
 }
--- lib/services/get_categorices.dart
+++ lib/services/get_categorices.dart
@@ -1,22 +1,17 @@
-import 'dart:convert';
-
-import 'package:http/http.dart' as http;
+import 'package:store/helper/api.dart';
 import 'package:store/models/get_model.dart';
 
 class GetCategoryServices {
   Future<List<ProductModel>> getCategory({required String categoryName}) async {
-    http.Response response = await http.get(
-        Uri.parse('https://fakestoreapi.com/products/category/$categoryName'));
-    if (response.statusCode == 200) {
-      List<dynamic> data = jsonDecode(response.body);
+    List<dynamic> data =
+      await  Api().get(url:'https://fakestoreapi.com/products/category/$categoryName');
+   
+      
       List<ProductModel> category = [];
       for (int i = 0; i < data.length; i++) {
         category.add(ProductModel.fromJson(data[i]));
       }
       return category;
-    } else {
-      throw Exception(
-          'there is aproblem with status code ${response.statusCode}');
-    }
+   
   }
 }
