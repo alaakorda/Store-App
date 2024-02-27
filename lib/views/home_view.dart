9b71c1adca3d5d9c8c687cfa39fcb6ca5e617842
alaakorda <alaazakaria436@gmail.com>
2024-02-27 08:26:41 +0200

    add product

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -23,47 +23,66 @@ class HomeView extends StatelessWidget {
         ],
       ),
       body: Center(
-          child: Container(
-        width: 250,
-        height: 150,
-        decoration:
-            BoxDecoration(borderRadius: BorderRadius.circular(20), boxShadow: [
-          BoxShadow(
-              blurRadius: 50,
-              color: const Color.fromARGB(255, 219, 218, 218).withOpacity(0.1),
-              spreadRadius: 10,
-              offset: const Offset(10, 10)),
-        ]),
-        child: const Card(
-          elevation: 8,
-          color: Colors.white,
-          child: Padding(
-            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Hand Bag LV',
-                    style: TextStyle(color: Colors.grey, fontSize: 18),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Stack(
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              width: 230,
+              height: 150,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 50,
+                        color: const Color.fromARGB(255, 219, 218, 218)
+                            .withOpacity(0.1),
+                        spreadRadius: 10,
+                        offset: const Offset(1, 1)),
+                  ]),
+              child: const Card(
+                elevation: 5,
+                color: Colors.white,
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(r'$255'),
-                      Icon(
-                        Icons.favorite,
-                        color: Colors.red,
+                      Text(
+                        'Hand Bag LV',
+                        style: TextStyle(color: Colors.grey, fontSize: 18),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(r'$255'),
+                          Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          )
+                        ],
                       )
                     ],
-                  )
-                ]),
-          ),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 50,
+              left: 80,
+              child: Image.asset(
+                'assets/vecteezy_isolated-black-t-shirt-white-hanger_8847294.png',
+                width: 200,
+                height: 200,
+              ),
+            ),
+          ],
         ),
-      )),
+      ),
     );
   }
 }
