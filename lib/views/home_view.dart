836dca9faaf251c5d8196e03001c04eca415687c
alaakorda <alaazakaria436@gmail.com>
2024-02-27 07:57:15 +0200

    added card

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -8,7 +8,7 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: AppBar(
+      appBar: AppBar(
         title: const Text('New Trend'),
         centerTitle: true,
         elevation: 0,
@@ -22,6 +22,48 @@ class HomeView extends StatelessWidget {
           )
         ],
       ),
+      body: Center(
+          child: Container(
+        width: 250,
+        height: 150,
+        decoration:
+            BoxDecoration(borderRadius: BorderRadius.circular(20), boxShadow: [
+          BoxShadow(
+              blurRadius: 50,
+              color: const Color.fromARGB(255, 219, 218, 218).withOpacity(0.1),
+              spreadRadius: 10,
+              offset: const Offset(10, 10)),
+        ]),
+        child: const Card(
+          elevation: 8,
+          color: Colors.white,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hand Bag LV',
+                    style: TextStyle(color: Colors.grey, fontSize: 18),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(r'$255'),
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      )
+                    ],
+                  )
+                ]),
+          ),
+        ),
+      )),
     );
   }
 }
